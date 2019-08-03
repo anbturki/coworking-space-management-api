ebdf5cacede9e0b000fed17e507c152018b6a397
Barakat Turki <barakat.turki@gmail.com>
2019-08-03 03:39:59 +0200

    add custom validation rules phone and exists rule

---
+++ start/hooks.js
@@ -0,0 +1,46 @@
+const { hooks } = require("@adonisjs/ignitor");
+const phone = require("phone");
+hooks.after.providersBooted(() => {
+  const Validator = use("Validator");
+  const Database = use("Database");
+
+  const existsFn = async (data, field, message, args, get) => {
+    const value = get(data, field);
+    if (!value) {
+      /**
+       * skip validation if value is not defined. `required` rule
+       * should take care of it.
+       */
+      return;
+    }
+
+    const [table, column] = args;
+    const row = await Database.table(table)
+      .where(column, value)
+      .first();
+
+    if (!row) {
+      throw message;
+    }
+  };
+
+  const phoneFn = async (data, field, message, args, get) => {
+    const value = get(data, field);
+    if (!value) {
+      /**
+       * skip validation if value is not defined. `required` rule
+       * should take care of it.
+       */
+      return;
+    }
+
+    const [countryCode] = args;
+    if (!phone || !phone(value, countryCode).length) {
+      throw message;
+    }
+  };
+
+  Validator.extend("exists", existsFn);
+  Validator.extend("phone", phoneFn);
+  // Database.on("query", console.log);
+});
