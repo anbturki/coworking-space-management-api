459a7ef1ef0565c84736c73ba0a700670b4f3703
Barakat Turki <barakat.turki@gmail.com>
2019-08-03 16:37:33 +0200

    implement a middleware for access control

---
+++ app/Middleware/Acl.js
@@ -0,0 +1,70 @@
+"use strict";
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class Acl {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle({ request, params, response, auth }, next, props) {
+    let { user } = auth;
+    const [model] = props;
+    let role = await user.role().fetch();
+    user = user.toJSON();
+    role = role.toJSON();
+    let permissions = {};
+    try {
+      permissions = JSON.parse(role.permissions);
+    } catch (error) {}
+    const segment = `/api/${model}`;
+    const method = request.method();
+    const url = request.url();
+    // Check if model match the current URL request
+    // FetchAll Request
+    if (url === segment && method === "GET") {
+      // Check if there is permissions
+      Acl.hasAccess(permissions, model, response, "fetchAll", next);
+    }
+    // FetchOne Request
+    else if (url === `${segment}/${params.id}` && method === "GET") {
+      Acl.hasAccess(permissions, model, response, "fetchOne", next);
+    }
+    // Store Request
+    else if (url === `${segment}` && method === "POST") {
+      Acl.hasAccess(permissions, model, response, "create", next);
+    }
+    // Update Request
+    else if (
+      url === `${segment}/${params.id}` &&
+      ["PATCH", "PUT"].includes(method)
+    ) {
+      Acl.hasAccess(permissions, model, response, "update", next);
+    }
+    // DELETE REQUEST
+    else if (url === `${segment}/${params.id}` && ["DELETE"].includes(method)) {
+      Acl.hasAccess(permissions, model, response, "delete", next);
+    }
+    // call next to advance the request
+    await next();
+  }
+  // Check if staff has action to access the route
+  static hasAccess(permissions, model, response, action, next) {
+    // Check if there is permissions
+    if (!Object.keys(permissions).length) {
+      return response.status(401).send("You are not authorized.");
+    }
+    if (
+      Array.isArray(permissions[model]) &&
+      permissions[model].includes(action)
+    ) {
+      return next();
+    } else {
+      return response.status(401).send("You are not authorized.");
+    }
+  }
+}
+
+module.exports = Acl;
