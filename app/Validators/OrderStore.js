e803fcc0ee9dfdb6f65548fde19faccfa5487b30
Barakat Turki <barakat.turki@gmail.com>
2019-08-07 15:57:15 +0200

    add validator for order routes

---
+++ app/Validators/OrderStore.js
@@ -0,0 +1,21 @@
+"use strict";
+const BaseValidator = use("App/Validators/BaseValidator");
+class OrderStore extends BaseValidator {
+  get rules() {
+    return {
+      item_id: "required|exists:stocks,id",
+      qty: "required|number",
+      session_id: "requiredWhen:order_type,SESSION|exists:sessions,id",
+      staff_id: "requiredWhen:order_type,STAFF|exists:users,id",
+      member_id: "requiredWhen:order_type,MEMBER|exists:users,id",
+      order_type: "required|in:SESSION,STAFF,MEMBER,GUEST"
+    };
+  }
+  get sanitizationRules() {
+    return {
+      qty: "to_int"
+    };
+  }
+}
+
+module.exports = OrderStore;
