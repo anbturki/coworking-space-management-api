165b9035575f7da6c69cdbca910d16af0b68cdb8
Barakat Turki <barakat.turki@gmail.com>
2019-08-07 12:50:04 +0200

    create UPDATE AND STORE validation middleware for sessions store and udpate

---
+++ app/Validators/SessionStore.js
@@ -0,0 +1,16 @@
+"use strict";
+const BaseValidator = use("App/Validators/BaseValidator");
+class SessionStore extends BaseValidator {
+  get rules() {
+    return {
+      work_day: "required|exists:work_days,id",
+      checked_in_at: "date",
+      code: "required",
+      type: "required|in:SHARED_SPACE,ROOM",
+      bundle_id: "required|exists:bundles,id",
+      location_id: "required|exists:locations,id"
+    };
+  }
+}
+
+module.exports = SessionStore;
---
+++ app/Validators/SessionUpdate.js
@@ -0,0 +1,17 @@
+"use strict";
+const BaseValidator = use("App/Validators/BaseValidator");
+class SessionStore extends BaseValidator {
+  get rules() {
+    return {
+      work_day: "exists:work_days,id",
+      checked_in_at: "date",
+      checked_in_out: "date",
+      type: "in:SHARED_SPACE,ROOM",
+      status: "in:ACTIVE,CLOSED",
+      bundle_id: "exists:bundles,id",
+      location_id: "exists:locations,id"
+    };
+  }
+}
+
+module.exports = SessionStore;
