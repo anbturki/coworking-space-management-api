7737c8d72616997545129ba8efe6dc919df4548a
Barakat Turki <barakat.turki@gmail.com>
2019-08-25 15:58:53 +0200

    calculate orders cost| calculate VIP_DESK type session

--- app/Models/Session.js
+++ app/Models/Session.js
@@ -3,6 +3,7 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
 const Bundle = use("App/Models/Bundle");
+const Order = use("App/Models/Order");
 const moment = require("moment");
 class Session extends Model {
   static get filters() {
@@ -32,6 +33,8 @@ class Session extends Model {
         if (session.status === "CLOSED") {
           return;
         }
+        const orderCost = await Session.calcOrdersPrice(session.id);
+        session.orders_cost = orderCost;
         const bundle = await Session.getSessionBundle(session);
         session.time_cost = (bundle && bundle.price) || 0;
       }
@@ -41,6 +44,8 @@ class Session extends Model {
       if (session.status === "CLOSED") {
         return;
       }
+      const orderCost = await Session.calcOrdersPrice(session.id);
+      session.orders_cost = orderCost;
       const bundle = await Session.getSessionBundle(session);
       session.time_cost = (bundle && bundle.price) || 0;
     });
@@ -68,6 +73,8 @@ class Session extends Model {
       return Session.handleSharedSpaceBundle(session);
     } else if (session.type === "ROOM") {
       return Session.handleRoomBundle(session);
+    } else if (session.type === "VIP_DESK") {
+      return Session.handleVipDesk(session);
     }
   }
   static async handleRoomBundle(session) {
@@ -99,6 +106,37 @@ class Session extends Model {
     }
     return currentBundle;
   }
+  static async handleVipDesk(session) {
+    // check if there is bundle for this time
+    let currentBundle = await Bundle.query()
+      .where({ co_working_hours: session.stayed_hours, type: "VIP_DESK" })
+      .first();
+    if (!currentBundle) {
+      // otherwise it look for the max price
+      return await Bundle.query()
+        .max({
+          price: "price"
+        })
+        .where("type", "VIP_DESK")
+        .first();
+    }
+    return currentBundle;
+  }
+  static async calcOrdersPrice(sessionId) {
+    let orders = await Order.query()
+      .where("session_id", sessionId)
+      .fetch();
+    orders = orders.toJSON();
+    if (!orders || !orders.length) {
+      return 0;
+    }
+    let qty = 0;
+    orders.forEach(order => {
+      qty += order.item_price * order.qty;
+    });
+    return qty;
+  }
+
   static calculateDuration(time) {
     const startTime = moment(time);
     const endTime = moment(new Date());
